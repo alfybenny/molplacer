3e99c604c9deee6bf3b6ba0ef40329ed39abaedb
alfybenny <ab8483@princeton.edu>
2021-11-12 00:51:51 -0500

    readme with links!

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # molplacer
+
+To translate and create multiple copies of molecules in 3d space.
+
+Object oriented programming tutorials important links:
+
+https://medium.com/@s.martinallo/abstract-virtual-classes-with-python-48bf60d00d9e
--- input.py
+++ input.py
@@ -5,12 +5,13 @@ class input:
     def __init__(self, filetype):
         self.filetype = filetype
 
-    def readfile(filename)
-            # This could also be a virtual function
-            # NOTE: To use the attribute defined above (constructor), we can use self.filetype here
-            # Read the filename with corresponding filetype attribute
-            # Do something with the read matrix based on the filetype attribute
-            # Retuen the atom list and coordinates
+    def readfile(filename):
+        # This could also be a virtual function
+        # NOTE: To use the attribute defined above (constructor), we can use self.filetype here
+        # Read the filename with corresponding filetype attribute
+        # Do something with the read matrix based on the filetype attribute
+        # Retuen the atom list and coordinates
+        pass
 
 class xyz(input):
 
